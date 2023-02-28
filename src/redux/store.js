bad4d01adb0253910bd98540ec76aa8b60611ea0
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-28 23:17:16 +0100

    Add store

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+// src/redux/store.js
+
+import { configureStore } from '@reduxjs/toolkit';
+import greetingReducer from './reducers/greetingReducer';
+
+const store = configureStore({
+  reducer: {
+    greeting: greetingReducer
+  }
+});
+
+export default store;
