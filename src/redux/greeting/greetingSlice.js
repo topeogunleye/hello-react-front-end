553539b52390b1e0633a9f46cc44ac04c9e4b87b
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-03 15:14:31 +0100

    Add GreetingSlice

---
+++ src/redux/greeting/greetingSlice.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// API URL
+const API_URL = 'http://127.0.0.1:8000/api/random_greeting';
+
+// Action Thunk
+export const fetchGreeting = createAsyncThunk('fetchMessages', async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json()
+  return data
+});
+
+// Reducers
+const messagesSlice = createSlice({
+  name: 'messages',
+  initialState: [],
+  extraReducers: (builder) => {
+    builder.addCase(
+      fetchGreeting.fulfilled,
+      (state, action) => action.payload,
+    );
+  },
+});
+
+export default messagesSlice.reducer;
