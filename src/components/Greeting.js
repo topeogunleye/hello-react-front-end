12916b954e4b4bc9d9219a29609af00a3e552765
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-03-03 15:13:53 +0100

    Add Greeting.js

---
+++ src/components/Greeting.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/greeting/greetingSlice';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  let greeting = useSelector((state) => state.greeting);
+
+  if (greeting === null || greeting === undefined) {
+    greeting = {}
+  }
+  console.log(greeting)
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  const postGreeting = () => {
+    if (greeting.message) {
+      return <h1>{greeting.message}</h1>
+    }
+    return <h1>Loading...</h1>
+  };
+
+
+  return (
+    <div>
+      {postGreeting()}
+    </div>
+  );
+};
+
+export default Greeting;
